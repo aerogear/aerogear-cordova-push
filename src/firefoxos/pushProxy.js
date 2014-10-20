e7ad7b5c4b65ad047f225cac12de2260ac282161
Lukas Fryc <lukas@fryc.eu>
2014-10-20 11:58:55 +0200

    manage registrations properly
    - unregister from all registrations except a first one
    - register new endpoint only when required
    - refresh existing registrations with UPS

--- src/firefoxos/pushProxy.js
+++ src/firefoxos/pushProxy.js
@@ -4,28 +4,47 @@ var UnifiedPushPlugin = require("org.jboss.aerogear.cordova.push.AeroGear.Unifie
 module.exports = {
   register: function (onNotification, fail, args) {
     var config = args[0];
-    var pushConfig = config.firefoxos || config;
-    if (!pushConfig || !pushConfig.variantID || !pushConfig.variantSecret || !config.pushServerURL) {
-      throw new Error('Incorrect push plugin configuration: ' + JSON.stringify( pushConfig ));
+    if (!config || !config.pushServerURL || !config.firefoxos || !config.firefoxos.variantID || !config.firefoxos.variantSecret) {
+      throw new Error('Incorrect push plugin configuration: ' + JSON.stringify( config ));
     }
 
-    var client = AeroGear.UnifiedPushClient(pushConfig.variantID, pushConfig.variantSecret, config.pushServerURL);
+    var client = AeroGear.UnifiedPushClient(config.firefoxos.variantID, config.firefoxos.variantSecret, config.pushServerURL);
 
-    var req = navigator.push.register();
-    req.onsuccess = function (e) {
+    var registrations = navigator.push.registrations();
+    registrations.onerror = fail;
+    registrations.onsuccess = function(e) {
+      if (registrations.result.length > 1) {
+        // clean registrations if there are more than one
+        for (var i = 1, l = registrations.result.length; i < l; i++) {
+          var pushEndpoint = registrations.result[i].pushEndpoint;
+          navigator.push.unregister(pushEndpoint);
+          client.unregisterWithPushServer(pushEndpoint);
+        }
+      }
+
       var settings = {
         success: UnifiedPushPlugin.successCallback,
         error: function (response, error) {
           fail(error);
+        },
+        metadata: {
+          alias: config.alias
         }
       };
 
-      var metadata = {
-        deviceToken: req.result,
-        alias: pushConfig.alias
-      };
-      settings.metadata = metadata;
-      client.registerWithPushServer(settings);
+      if (registrations.result.length > 0) {
+        // use existing registration
+        settings.metadata.deviceToken = registrations.result[0].pushEndpoint;
+        client.registerWithPushServer(settings);
+      } else {
+        // new registration
+        var register = navigator.push.register();
+        register.onerror = fail;
+        register.onsuccess = function () {
+          settings.metadata.deviceToken = register.result;
+          client.registerWithPushServer(settings);
+        };
+      }
     };
 
     navigator.mozSetMessageHandler('push', function (message) {
