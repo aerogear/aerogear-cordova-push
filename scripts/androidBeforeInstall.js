a67efbad048326ad8139762df3d5c5a7cc6e05e6
Olivier Thierry <othierry@asi.fr>
2016-08-01 14:54:05 +0200

    AGCORDOVA-159 : Use readline module in androidBeforeInstall.js

--- scripts/androidBeforeInstall.js
+++ scripts/androidBeforeInstall.js
@@ -2,6 +2,7 @@ module.exports = function(ctx) {
     var fs = ctx.requireCordovaModule('fs'),
         path = ctx.requireCordovaModule('path'),
         os = require("os"),
+        readline = require("readline"),
         deferral = ctx.requireCordovaModule('q').defer();
 
     var platformRoot = path.join(ctx.opts.projectRoot, 'www');
@@ -11,9 +12,13 @@ module.exports = function(ctx) {
         if (err) {
             deferral.reject("To use this plugin on android you'll need to add a google-services.json file with the FCM project_info and place that into your www folder");
         } else {
+
             fs.createReadStream(settingsFile).pipe(fs.createWriteStream('platforms/android/google-services.json'));
 
-            fs.readFileSync('platforms/android/build.gradle').toString().split(os.EOL).forEach(function (line) {
+            var lineReader = readline.createInterface({
+                input : fs.createReadStream('platforms/android/build.gradle')
+            });
+            lineReader.on("line", function(line) {
                 fs.appendFileSync('./build.gradle', line.toString() + os.EOL);
                 if (/.*\ dependencies \{.*/.test(line)) {
                     fs.appendFileSync('./build.gradle', '\t\tclasspath "com.google.gms:google-services:3.0.0"' + os.EOL);
