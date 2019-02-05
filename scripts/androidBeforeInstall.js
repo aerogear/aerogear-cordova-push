2801ffb772ff63c3d5938daf12ebfb6071e5a7c9
Paulo Santana <pcsantana2909@gmail.com>
2019-02-05 14:49:19 -0200

    Fix compatibility Cordova Android 7

--- scripts/androidBeforeInstall.js
+++ scripts/androidBeforeInstall.js
@@ -13,11 +13,11 @@ module.exports = function(ctx) {
             deferral.reject("To use this plugin on android you'll need to add a google-services.json file with the FCM project_info and place that into your www folder");
         } else {
 
-            fs.createReadStream(settingsFile).pipe(fs.createWriteStream('platforms/android/google-services.json'));
+            fs.createReadStream(settingsFile).pipe(fs.createWriteStream('platforms/android/app/google-services.json'));
 
             var lineReader = readline.createInterface({
                 terminal: false,
-                input : fs.createReadStream('platforms/android/build.gradle')
+                input : fs.createReadStream('platforms/android/app/build.gradle')
             });
             lineReader.on("line", function(line) {
                 fs.appendFileSync('./build.gradle', line.toString() + os.EOL);
@@ -26,7 +26,7 @@ module.exports = function(ctx) {
                     fs.appendFileSync('./build.gradle', '\t\tclasspath "com.android.tools.build:gradle:1.2.3+"' + os.EOL);
                 }
             }).on("close", function () {
-                fs.rename('./build.gradle', 'platforms/android/build.gradle', deferral.resolve);
+                fs.rename('./build.gradle', 'platforms/android/app/build.gradle', deferral.resolve);
             });
 
         }
