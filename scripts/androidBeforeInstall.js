6afbb4f4d3dd1d8a55fc3264f601fd569647f45b
Paulo Santana <pcsantana@celepar.pr.gov.br>
2021-02-02 18:26:59 -0300

    Removed the pre condition to have google-services.json in www folder

--- scripts/androidBeforeInstall.js
+++ scripts/androidBeforeInstall.js
@@ -1,11 +1,7 @@
 module.exports = function(ctx) {
     var fs = require('fs'),
-        path = require('path'),
         os = require("os"),
         readline = require("readline");
-
-    var platformRoot = path.join(ctx.opts.projectRoot, 'www');
-    var settingsFile = path.join(platformRoot, 'google-services.json');
 
     var platformAndroid = 'platforms/android';
 
@@ -15,28 +11,18 @@ module.exports = function(ctx) {
     }
 
     return new Promise(function(resolve, reject) {
-        fs.stat(settingsFile, function(err,stats) {
-            if (err) {
-                reject("To use this plugin on android you'll need to add a google-services.json file with the FCM project_info and place that into your www folder");
-            } else {
-
-                fs.createReadStream(settingsFile).pipe(fs.createWriteStream(platformAndroid + '/google-services.json'));
-
-                var lineReader = readline.createInterface({
-                    terminal: false,
-                    input : fs.createReadStream(platformAndroid + '/build.gradle')
-                });
-                lineReader.on("line", function(line) {
-                    fs.appendFileSync('./build.gradle', line.toString() + os.EOL);
-                    if (/.*\ dependencies \{.*/.test(line)) {
-                        fs.appendFileSync('./build.gradle', '\t\tclasspath "com.google.gms:google-services:4.2.0"' + os.EOL);
-                        fs.appendFileSync('./build.gradle', '\t\tclasspath "com.android.tools.build:gradle:3.3.0"' + os.EOL);
-                    }
-                }).on("close", function () {
-                    fs.rename('./build.gradle', platformAndroid + '/build.gradle', resolve);
-                });
-
-            }
-        });
+		var lineReader = readline.createInterface({
+			terminal: false,
+			input : fs.createReadStream(platformAndroid + '/build.gradle')
+		});
+		lineReader.on("line", function(line) {
+			fs.appendFileSync('./build.gradle', line.toString() + os.EOL);
+			if (/.*\ dependencies \{.*/.test(line)) {
+				fs.appendFileSync('./build.gradle', '\t\tclasspath "com.google.gms:google-services:4.2.0"' + os.EOL);
+				fs.appendFileSync('./build.gradle', '\t\tclasspath "com.android.tools.build:gradle:3.3.0"' + os.EOL);
+			}
+		}).on("close", function () {
+			fs.rename('./build.gradle', platformAndroid + '/build.gradle', resolve);
+		});
     })
 };
